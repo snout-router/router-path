08819d23c756f5dde5a07dc9347499aadeb961d7
Erin Millard <ezzatron@gmail.com>
2021-03-19 10:12:15 +1000

    Use @packula/jest-config

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,1 @@
-module.exports = {
-  preset: 'ts-jest',
-  collectCoverageFrom: [
-    'src/**/*.ts',
-  ],
-  coverageDirectory: 'artifacts/coverage/jest',
-  testEnvironment: 'node',
-  testMatch: [
-    '**/test/unit/**/*.spec.ts',
-  ],
-}
+module.exports = require('@packula/jest-config')
