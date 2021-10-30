48045efd392e36f653bb3239dd07e0897e7b0717
Erin Millard <ezzatron@gmail.com>
2021-10-30 14:06:07 +1000

    Fix missing TS type information

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
 import {escape} from '@snout/regexp'
 
-import {AllowOmitUndefined, Cast} from './types'
-
 export type AnyParam = Param<string, any>
 
 export function normalizeParam<P extends ParamOrString> (p: P): NormalizeParam<P> {
@@ -117,3 +115,32 @@ type Args<Params extends AnyParams> = AllowOmitUndefined<Result<Params>> & { [Ke
 type NormalizeParams<Params extends ParamsOrStrings> = {
   [Key in keyof Params]: NormalizeParam<Cast<Params[Key], ParamOrString>>
 }
+
+// types -----------------------------------------------------------------------
+
+/**
+ * Constructs a type with all properties of Type that accept undefined values set to optional
+ *
+ * Similar to the built-in Partial<Type> utility type, but only applies the optional modifier to properties that also
+ * accept "undefined"
+ */
+ type AllowOmitUndefined<Type extends {}> =
+  { [Key in RequiredKeys<Type>]: Type[Key] } &
+  { [Key in OptionalKeys<Type>]+?: Type[Key] }
+
+/**
+* Returns Type if it extends Subtype, otherwise returns Subtype
+*
+* Inspired by https://dev.to/svehla/typescript-object-fromentries-389c
+*/
+type Cast<Type, Subtype> = Type extends Subtype ? Type : Subtype
+
+/**
+* Finds the keys of an object literal type which do *not* accept undefined values, and returns a union of the key types
+*/
+type RequiredKeys<Type extends {}> = { [Key in keyof Type]: undefined extends Type[Key] ? never : Key }[keyof Type]
+
+/**
+* Finds the keys of an object literal type which accept undefined values, and returns a union of the key types
+*/
+type OptionalKeys<Type extends {}> = { [Key in keyof Type]: undefined extends Type[Key] ? Key : never }[keyof Type]
